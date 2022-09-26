0bec34b7fd12f18782312ddc61467f9201500171
jmhmjayamaha <jmhmjayamaha@gmail.com>
2022-09-26 06:30:50 +0530

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO drone_details (id,serial_number, model,weight_limit,battery_capacity,state) VALUES (1,'asdfddec1235','Lightweight',400,90,'LOADING');
-INSERT INTO medication_details (id, name, weight,code,image_location,drone_id) VALUES (1,'suger1 tester',100,'3r4s','http://locations',1);
+INSERT INTO drone_details (id,serial_number, model,weight_limit,battery_capacity,state) VALUES (1,'4cgtevV3suvUq3TeMtsv','Lightweight',400,90,'LOADING');
+INSERT INTO medication_details (id, name, weight,code,image_location,drone_id) VALUES (1,'Voltaren_Gel',100,'MEDIC001','http://locations',1);
 
 
-INSERT INTO drone_details (id,serial_number, model,weight_limit,battery_capacity,state) VALUES (2,'asdfddec123df5','Middleweight',400,90,'DELIVERING');
-INSERT INTO medication_details (id, name, weight,code,image_location,drone_id) VALUES (2,'suger tester',100,'3r4s','http://locations',2);
-INSERT INTO medication_details (id, name, weight,code,image_location,drone_id) VALUES (3,'test tester',100,'3r4s','http://locations',2);
+INSERT INTO drone_details (id,serial_number, model,weight_limit,battery_capacity,state) VALUES (2,'Qik8R5N6FUFhi6AFN4Sq','Middleweight',400,90,'DELIVERING');
+INSERT INTO medication_details (id, name, weight,code,image_location,drone_id) VALUES (2,'Gabapentin',100,'MEDIC121','http://locations',2);
+INSERT INTO medication_details (id, name, weight,code,image_location,drone_id) VALUES (3,'Gonak_Hypromellose',100,'MEDIC123','http://locations',2);
 
-INSERT INTO drone_details (id,serial_number, model,weight_limit,battery_capacity,state) VALUES (3,'asdfddec1sdf235','Lightweight',400,24,'IDLE');
+INSERT INTO drone_details (id,serial_number, model,weight_limit,battery_capacity,state) VALUES (3,'mzcgqPwDF7YiX96EGUu9','Lightweight',400,24,'IDLE');
