12284c17eb5ca294e352536bab7585c7cde0eaab
jmhmjayamaha <jmhmjayamaha@gmail.com>
2022-10-01 07:58:19 +0530

    swagger ui implementation

--- src/main/java/com/musalasoft/application/MusalasoftDroneApplication.java
+++ src/main/java/com/musalasoft/application/MusalasoftDroneApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "Drone project APIs", version = "1.0", description = "This project is created for testing purpose"))
 public class MusalasoftDroneApplication {
 
 	public static void main(String[] args) {
